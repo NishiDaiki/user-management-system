a3c800772e64fc377c73d1a451ce449c87fc3a68
NishiDaiki <daiki.nishi@fox-hound.jp>
2025-12-09 12:01:37 +0900

    task1-2完了

---
+++ components/RegisterForm.stories.tsx
@@ -0,0 +1,26 @@
+// components/RegisterForm.stories.tsx
+
+import type { Meta, StoryObj } from "@storybook/react";
+import RegisterForm from "./RegisterForm";
+
+// TODO: メタデータ
+
+const meta: Meta<typeof RegisterForm> = {
+  title: "Components/RegisterForm",
+  component: RegisterForm,
+};
+
+export default meta;
+
+// TODO: ストーリーの定義
+
+type Story = StoryObj<typeof RegisterForm>;
+
+// TODO: デフォルトストーリーの設定
+
+export const Default: Story = {
+  args: {
+    onSuccess: () => alert("登録成功（onSuccess）がよばれました"),
+    onError: () => alert("エラー（onError）がよばれました"),
+  },
+};
