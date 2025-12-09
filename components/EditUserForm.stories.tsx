46f781efc31e66531006e8d1a6c37e45fe16159c
NishiDaiki <daiki.nishi@fox-hound.jp>
2025-12-09 14:35:33 +0900

    task2-2完了

---
+++ components/EditUserForm.stories.tsx
@@ -0,0 +1,26 @@
+// components/EditUserForm.stories.tsx
+
+import type { Meta, StoryObj } from "@storybook/react";
+import EditUserForm from "./EditUserForm";
+
+// TODO: メタデータ
+
+const meta: Meta<typeof EditUserForm> = {
+  title: "Components/EditUserForm",
+  component: EditUserForm,
+};
+
+export default meta;
+
+// TODO: ストーリーの定義
+
+type Story = StoryObj<typeof EditUserForm>;
+
+//デフォルトストーリー
+export const Default: Story = {
+  args: {
+    userId: "1", // 例となるユーザーID
+    onSuccess: () => alert("更新成功"),
+    onError: (error) => alert(`エラー:' ${error}`),
+  },
+};
