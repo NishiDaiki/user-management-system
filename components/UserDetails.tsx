968e445f01c6f11a01bffa76fd3414abf7ce71e4
NishiDaiki <daiki.nishi@fox-hound.jp>
2025-12-10 14:43:24 +0900

    Day2task3-1完了

---
+++ components/UserDetails.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { User } from "../types/User";
+import { Card, CardContent, Typography } from "@mui/material";
+
+//インターフェイスの定義
+interface UserDetailsProps {
+  user: User;
+}
+const UserDetails: React.FC<UserDetailsProps> = ({ user }) => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="body1" sx={{ mb: 1 }}>
+          名前:{user.name}
+        </Typography>
+        <Typography variant="body1" sx={{ mb: 1 }}>
+          メールアドレス:{user.email}
+        </Typography>
+        <Typography variant="body1" sx={{ mb: 1 }}>
+          役職:{user.role}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
