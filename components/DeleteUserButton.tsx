66eccb0981f9e9c6096d576ddc5936cf31873cc1
NishiDaiki <daiki.nishi@fox-hound.jp>
2025-12-11 15:27:19 +0900

    CustomModalに差し替え、モーダル開閉のstate管理追加

--- components/DeleteUserButton.tsx
+++ components/DeleteUserButton.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
+import CustomModal from "./parts/CustomModal";
 
 //インターフェイス定義
 interface DeleteUserButtonProps {
@@ -11,10 +12,22 @@ const DeleteUserButton: React.FC<DeleteUserButtonProps> = ({
   userId,
   onDelete,
 }) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Button variant="outlined" color="error">
-      削除
-    </Button>
+    <>
+      <Button variant="outlined" color="error">
+        削除
+      </Button>
+
+      <CustomModal
+        open={open}
+        title="削除確認"
+        content="本当にこのユーザーを削除しますか？"
+        onClose={() => setOpen(false)}
+        onConfirm={handleConfirm}
+      ></CustomModal>
+    </>
   );
 };
 
